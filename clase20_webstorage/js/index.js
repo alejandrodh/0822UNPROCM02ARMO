8311785e3f4d6bdc754bb4e995f093552b4697c3
alejandrodh <alejandro@digitalhouse.com>
2022-10-28 07:47:27 -0300

    webstorage

---
+++ clase20_webstorage/js/index.js
@@ -0,0 +1,18 @@
+// Ejercicio de webstorage.
+console.log('Práctica');
+// 1. Crear la variable miNombre y guardá tu nombre en ella. 
+// 2. Guardá tu nombre en localStorage dentro de la propiedad 'userName'. 
+// 3. Verifiquemos que todo esté en su lugar: escribí console.log(localStorage) y deberías ver el contenido del storage en la consola. ¿Sabías que podés verificar esto mismo en el navegador? En las herramientas de desarrollo andá a Aplication >> Storage >> Local Storage
+// 4.Recuperá el dato de localStorage, guardalo dentro de la variable recuperoStorage y mostralo por consola.
+// 5. Borrá todo lo que tenga localStorage guardado en el navegador.
+
+
+//Practica 2
+let peliculas = ['IronMan', 'Star Wars','Rouge One']
+let peliculaFavorita = 
+
+// Guaradar en storage
+localStorage.setItem('peliculaFavorita',)
+console.log(localStorage);
+
+//Recuprarla
